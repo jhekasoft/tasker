a9d1df0f474cbe798f4103bb13be98ad012eefa4
bondvt04 <bondvt04@gmail.com>
2012-09-13 14:01:53 +0300

    Готова страница с выводом строк из БД

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,19 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=clients_repository5x;host=10.0.0.99',
+        'username'       => 'user',
+        'password'       => 'usbw',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
